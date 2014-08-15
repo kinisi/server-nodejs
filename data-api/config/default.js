52d5c8c8c7804d0488f6fbe2d7dedf6105d225f9
Trevor Erik Carlson <trevor@kinisi.cc>
2014-08-15 17:12:44 -0400

    [config] Refer to the main master database, ai1dev1.kinisi.cc

--- data-api/config/default.js
+++ data-api/config/default.js
@@ -2,9 +2,9 @@ module.exports = {
     "port": 5674,
     "mysql": {
         "conn": {
-            "host": "ai1dev1",
-            "user": "kuser0",
-            "password": "9aad1067f476ba",
+            "host": "ai1dev1.kinisi.cc",
+            "user": "kadm",
+            "password": "5ad50dcdc887b2",
             "database": "kdev1",
             "dateStrings": true
         }
