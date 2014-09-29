b52ab52ebace6d056087d7f1118c626fb2199d4e
transistorize <bryan@kinisi.cc>
2014-09-29 17:10:02 -0400

    [dashboard] WIP fix export form design, start adding event handlers

--- data-api/static/js/export.util.js
+++ data-api/static/js/export.util.js
@@ -2,36 +2,55 @@
 
 
 var FormatPicker = React.createClass({
+
+    getInitialState: function() {
+        return { format: "csv" };
+    },
+
     componentDidMount: function() {
         $('.btn', this.getDOMNode()).button();
     },
     
     render: function() {
-        return <div className="btn-group" data-toggle="buttons">
-            <label className="btn btn-default active">
-                <input type="radio" name="format_options" id="option_csv" defaultChecked /> CSV
-            </label>
-            <label className="btn btn-default">
-                <input type="radio" name="format_options" id="option_kml" /> KML
-            </label>
-        </div>;
+        return  <div className="k-format-picker container">
+           <div className="row">
+            <div className="col-sm-1"><label>Format</label></div>
+            <div className="col-sm-11">
+            <div className="btn-group" data-toggle="buttons">
+                <label className="btn btn-default active">
+                    <input type="radio" name="format_options" id="option_csv" defaultChecked /> CSV
+                </label>
+                <label className="btn btn-default">
+                    <input type="radio" name="format_options" id="option_kml" /> KML
+                </label>
+                <label className="btn btn-default">
+                    <input type="radio" name="format_options" id="option_json" /> JSON
+                </label>
+            </div></div>
+        </div></div>;
     }
 });
 
 var DatePicker = React.createClass({
+
+    getIntialState: function() {
+        return { rangeOption: "1 day" };
+    },
+    
     componentDidMount: function() {        
-        var from = "#" + this.props.name + "-from",
+        var component = this,
+            from = "#" + this.props.name + "-from",
             to = "#" + this.props.name + "-to",
             node = this.getDOMNode();
 
         $('.btn', node).button();
         
         $(from, node).datepicker({
-            defaultDate: "-1w",
+            defaultDate: "-1d",
             changeMonth: true,
             numberOfMonths: 1,
             onClose: function( selectedDate ) {
-                $(to, node).datepicker( "option", "minDate", selectedDate );
+                $(to, node).datepicker( "option", "minDate", selectedDate);
             }
         });
 
@@ -40,56 +59,66 @@ var DatePicker = React.createClass({
             changeMonth: true,
             numberOfMonths: 1,
             onClose: function( selectedDate ) {
-                $(from, node).datepicker( "option", "maxDate", selectedDate );
+                $(from, node).datepicker( "option", "maxDate", selectedDate);
             }
         });
     },
 
     render: function() {
-        return <div>
-   
-         Presets: 
-         <div className="btn-group" data-toggle="buttons">
-            <label className="btn btn-default active">
-                <input type="radio" name="range_options" id="option1" defaultChecked /> 1 day
-            </label>
-            <label className="btn btn-default">
-                <input type="radio" name="range_options" id="option2" /> 1 week 
-            </label>
-            <label className="btn btn-default">
-                <input type="radio" name="range_options" id="option3" /> 1 month 
-            </label>
+        return <div className="k-date-picker container">
+            <div className="row">
+                <div className="col-sm-1">         
+                    <label>Presets</label>    
+                </div>
+                <div className="btn-group col-sm-11" data-toggle="buttons">           
+                    <label className="btn btn-default active">
+                    <input type="radio" name="range_options" id="option1" defaultChecked />1 day</label>
+                    <label className="btn btn-default"><input type="radio" name="range_options" id="option2" />7 days</label>
+                    <label className="btn btn-default"><input type="radio" name="range_options" id="option3" />30 days</label>
+                    <label className="btn btn-default"><input type="radio" name="range_options" id="option4" />Custom</label>
+                </div>
+            </div>
 
-        </div>
-        <div>
-        <label htmlFor={this.props.name + "-from"}>From</label>
-        <input type="text" id={this.props.name + "-from"} name="from" className="form-control"/>
-        <label htmlFor={this.props.name + "-to"}>to</label>
-        <input type="text" id={this.props.name + "-to"} name="to" className="form-control"/>
-        </div>
-        </div>;
-    }
-});
+            <div className="row">
+                <div className="col-sm-11">
+                    <label>From <input type="text" id={this.props.name + "-from"} name="from" className="form-control" /></label>
+                    <label>to <input type="text" id={this.props.name + "-to"} name="to" className="form-control" /></label>
+                </div>
+            </div>
 
-var DeviceList = React.createClass({
-    render: function() {
-        return <div>DeviceList</div>;
+            </div>;
     }
 });
 
 
-var ExportBox = React.createClass({
+var ExportForm = React.createClass({
+    
+    handleSubmit: function(e) {
+        console.log(e);
+        e.preventDefault();
+        alert(JSON.stringify(e));
+    },
+
     render: function() {
-        return <div>
-    <DatePicker />
-    <hr/>
-    <FormatPicker />
-    <DeviceList />
-    <hr/>
-    <button className="btn btn-primary btn-default"> 
-        <span>{this.props.action} </span> 
-        <span className="glyphicon glyphicon-export"></span>
-    </button></div>;
+        return <form className="k-export-form panel panel-default" id={this.props.id} onSubmit={this.handleSubmit}>
+            <div className="panel-body container-fluid">
+                <div className="row">
+                    <div className="col-md-1"></div>
+                    <div className="col-md-5"><DatePicker parentForm={this.props.id} /></div>
+                    <div className="col-md-5"><FormatPicker parentForm={this.props.id} /></div>
+                </div>
+                <div className="row">
+                    <div className="col-md-1"></div>
+                    <div className="col-md-10">
+                    <hr/>
+                    <button type="submit" className="btn btn-primary btn-default"> 
+                        <span>{this.props.action} </span> 
+                        <span className="glyphicon glyphicon-export"></span>
+                    </button>
+                    </div>
+                </div>
+            </div>
+        </form>;
     }
 });
 
