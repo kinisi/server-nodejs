7709483b95ff1730169394f487852894528d5422
transistorize <bryan@kinisi.cc>
2014-10-14 13:12:05 -0400

    [auth] add api token to url redirects for calls to the data export api

--- data-api/routes/passport.js
+++ data-api/routes/passport.js
@@ -1,22 +1,12 @@
 var router = require("../lib/router");
-//var rutil = require("../lib/router_util");
-//var auth = require("../lib/middleware/auth");
-//var tablequery = require("../lib/middleware/tablequery");
 var AuthHandler = require("../lib/middleware/AuthHandler");
+var passport = require('passport');
+var google_strategy = require('passport-google-oauth').OAuth2Strategy;
+var db = require("../lib/db");
 
-//        app.get('/auth/google',handlers.auth.googleSignIn);
-//        app.get('/auth/google/callback',handlers.auth.googleSignInCallback);
-
-//router.add("GET", /\/table\//, auth, tablequery, rutil.writeCORSHeaders, rutil.writeJSONPBody);
 router.add("GET", /^\/auth\/google\/callback/, AuthHandler.googleSignInCallback);
 router.add("GET", /^\/auth\/google/, AuthHandler.googleSignIn);
 router.add("GET", /^\/$/, AuthHandler.googleSignIn);
-
-var passport = require('passport');
-
-var google_strategy = require('passport-google-oauth').OAuth2Strategy;
-
-var db = require("../lib/db");
 
 var saveToken = function(email, accessToken, callback) {
     db.query("insert into oauth_token (user_id, oauth_service, oauth_token, oauth_expires) select id, 'google', ?, date_add(now(), interval 1 hour) from api_token where email = ?",
@@ -27,13 +17,13 @@ var saveToken = function(email, accessToken, callback) {
 };
 
 passport.use(new google_strategy({
-    clientID: '584161997041-6rmosthf1f3vfl6u5se1gml5pv973va0.apps.googleusercontent.com',
-    clientSecret: 'db4T8HsTPF62gKmUlUF_QBZn',
-    callbackURL: 'https://maps.kinisi.cc/auth/google/callback'
-},
-function(accessToken, refreshToken, profile, done) {
-    saveToken(profile._json.email, accessToken, function(err) {
-        done(err, { "token": accessToken, "profile": profile });
-    });
-}
+        clientID: '584161997041-6rmosthf1f3vfl6u5se1gml5pv973va0.apps.googleusercontent.com',
+        clientSecret: 'db4T8HsTPF62gKmUlUF_QBZn',
+        callbackURL: 'https://maps.kinisi.cc/auth/google/callback'
+    },
+    function(accessToken, refreshToken, profile, done) {
+        saveToken(profile._json.email, accessToken, function(err) {
+            done(err, { "token": accessToken, "profile": profile });
+        });
+    }
 ));
--- data-api/static/js/dashboard.js
+++ data-api/static/js/dashboard.js
@@ -3,32 +3,27 @@
  * depends on jquery, react, bootstrap
  */
 
-var oauth_token = null;
-var userid = null;
-
-function initialize() {
-    var qs = location.search.slice(1).split("&").reduce(function(p,c) { c.split("=").every(function(k,i,a) { p[k] = a[i+1]; }); return p; }, {});
-
-    oauth_token = qs['token'];
-    userid = qs['userid'];
-
-    //$('#image_download').append($('<a>').prop({'href':'/download/rpi-image?userid='+userid+'&oauth_token='+oauth_token}).text('Download your rPi image here'));
-
+function initialize(userid, oauthToken, apiToken) {
+    var url = "/table/device_configuration?oauth_token=" + oauthToken + "&userid=" + userid +
+        "&c=api_token_id,device_id&f=api_token_id|eq|" + userid + "&o=device_id&limit=100"; // + QUERY_LIMIT;
 
     var to = setTimeout(function() {
-        var url = "/table/device_configuration?oauth_token=" + oauth_token + "&userid=" + userid + "&c=api_token_id,device_id&f=api_token_id|eq|" + userid + "&o=device_id&limit=100"; // + QUERY_LIMIT;
-
         $.getJSON(url, function(data) {
             $("#loading").hide();
-            makeDeviceTable(data);
+            makeDeviceTable(data, userid, oauthToken, apiToken);
         }).complete(function() { to = null; });
     }, 10);
-
 }
 
-function makeDeviceTable(data) {
+// currently disabled
+function createImageDownload(userid, oauthToken) {
+    $('#image_download').append($('<a>').prop({
+        'href':'/download/rpi-image?userid=' + userid + '&oauth_token=' + oauthToken})
+        .text('Download your rPi image here'));
+}
 
-    var DEVICE_ID = "kinisi-device-id";
+function makeDeviceTable(data, userid, oauthToken, apiToken) {
+    var DEVICE_ID_TAG = "kinisi-device-id";
 
     $('<tr>').append(
         $('<th>').text('Device ID (view map)'),
@@ -38,9 +33,11 @@ function makeDeviceTable(data) {
         $('<th>').text('MAC wlan'),
         $('<th>').text('MAC eth'),
         $('<th>').append(
-            $('<button>').prop({'class': 'btn btn-primary btn-xs', id: 'export_panel_button', 'disabled': true }).append(
-                $('<span>').text('Export '),
-                $('<span>').prop({'class': 'glyphicon glyphicon-export'})
+            // attach disabled export button, enable on checking a device
+            $('<button>').prop({'class': 'btn btn-primary btn-xs', id: 'export_panel_button', disabled: true })
+                .append(
+                    $('<span>').text('Export '),
+                    $('<span>').prop({'class': 'glyphicon glyphicon-export'})
             )
         )
     ).appendTo('#device_table');
@@ -48,7 +45,7 @@ function makeDeviceTable(data) {
 
     $.each(data, function(i, item) {
         var $tr = $('<tr>').append(
-            $('<td>').append($('<a>').prop({'href':'/static/testmap-osm.html?userid='+userid+'&token='+oauth_token+'&device_id='+item.device_id}).text(item.device_id)),
+            $('<td>').append($('<a>').prop({'href':'/static/testmap-osm.html?userid='+userid+'&token='+oauthToken+'&device_id='+item.device_id}).text(item.device_id)),
             $('<td>').prop({'id': item.device_id+'_measure'}),
             $('<td>').prop({'id': item.device_id+'_receive'}),
             $('<td>').prop({'id': item.device_id+'_entries'}),
@@ -56,12 +53,12 @@ function makeDeviceTable(data) {
             $('<td>').prop({'id': item.device_id+'_eth'}),
             $('<td>').prop({'id': item.device_id+'_export'}).append(
                 $('<input>').prop({type: 'checkbox', 'class': 'export'})
-                    .data(DEVICE_ID, item.device_id))
+                    .data(DEVICE_ID_TAG, item.device_id))
         ).appendTo('#device_table');
     });
 
     $.each(data, function(i, item) {
-        var url = "/table/device_location?oauth_token=" + oauth_token + "&userid=" + userid + "&c=measure_time&f=device_id|eq|" + item.device_id + "&o=measure_time%20desc&limit=1";
+        var url = "/table/device_location?oauth_token=" + oauthToken + "&userid=" + userid + "&c=measure_time&f=device_id|eq|" + item.device_id + "&o=measure_time%20desc&limit=1";
         $.getJSON(url, function(data) {
             var cell_text;
             if (data && data[0] && ("measure_time" in data[0])) {
@@ -74,7 +71,7 @@ function makeDeviceTable(data) {
     });
 
     $.each(data, function(i, item) {
-        var url = "/table/device_location?oauth_token=" + oauth_token + "&userid=" + userid + "&c=receive_time&f=device_id|eq|" + item.device_id + "&o=receive_time%20desc&limit=1";
+        var url = "/table/device_location?oauth_token=" + oauthToken + "&userid=" + userid + "&c=receive_time&f=device_id|eq|" + item.device_id + "&o=receive_time%20desc&limit=1";
         $.getJSON(url, function(data) {
             var cell_text;
             if (data && data[0] && ("receive_time" in data[0])) {
@@ -87,8 +84,8 @@ function makeDeviceTable(data) {
     });
 
     $.each(data, function(i, item) {
-        //var url = "/table/device_interface?oauth_token=" + oauth_token + "&userid=" + userid + "&c=address&f=device_id|eq|" + item.device_id + ";name|like|eth&o=address&limit=1";
-        var url = "/table/device_location?oauth_token=" + oauth_token + "&userid=" + userid + "&f=device_id|eq|" + item.device_id + ";latitude|nnull;longitude|nnull&G=count|device_id";
+        //var url = "/table/device_interface?oauth_token=" + oauthToken + "&userid=" + userid + "&c=address&f=device_id|eq|" + item.device_id + ";name|like|eth&o=address&limit=1";
+        var url = "/table/device_location?oauth_token=" + oauthToken + "&userid=" + userid + "&f=device_id|eq|" + item.device_id + ";latitude|nnull;longitude|nnull&G=count|device_id";
         $.getJSON(url, function(data) {
             var cell_text;
             if (data && data[0] && ("count(device_id)" in data[0])) {
@@ -101,7 +98,7 @@ function makeDeviceTable(data) {
     });
 
     $.each(data, function(i, item) {
-        var url = "/table/device_interface?oauth_token=" + oauth_token + "&userid=" + userid + "&c=address&f=device_id|eq|" + item.device_id + ";name|like|wlan&o=address&limit=1";
+        var url = "/table/device_interface?oauth_token=" + oauthToken + "&userid=" + userid + "&c=address&f=device_id|eq|" + item.device_id + ";name|like|wlan&o=address&limit=1";
         $.getJSON(url, function(data) {
             var cell_text;
             if (data && data[0] && ("address" in data[0])) {
@@ -114,7 +111,7 @@ function makeDeviceTable(data) {
     });
 
     $.each(data, function(i, item) {
-        var url = "/table/device_interface?oauth_token=" + oauth_token + "&userid=" + userid + "&c=address&f=device_id|eq|" + item.device_id + ";name|like|eth&o=address&limit=1";
+        var url = "/table/device_interface?oauth_token=" + oauthToken + "&userid=" + userid + "&c=address&f=device_id|eq|" + item.device_id + ";name|like|eth&o=address&limit=1";
         $.getJSON(url, function(data) {
             var cell_text;
             if (data && data[0] && ("address" in data[0])) {
@@ -127,14 +124,14 @@ function makeDeviceTable(data) {
     });
 
     // export panel
-    var formUrl = "https://api.kinisi.cc/geoserver/api/export?api_token=" + oauth_token;
+    var formUrl = "https://api.kinisi.cc/geoserver/api/export?api_token=" + apiToken;
     var exportBox = React.renderComponent(ExportForm({id: "export_form", action: "Submit", url: formUrl}), $('#export_panel').get(0));
 
     function enableExportView() {
         var checked = $(":checkbox.export").filter(function(i, elem) {
             return elem.checked;
         }).map(function(i, elem) {
-            return $(elem).data(DEVICE_ID);
+            return $(elem).data(DEVICE_ID_TAG);
         });
         var checkedForAny = checked.length > 0;
         $("#export_panel_button").attr("disabled", !checkedForAny && $("#export_panel").hasClass("hidden") );
